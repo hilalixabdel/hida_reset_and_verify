b2ef6566ee07bcf6f84edb66c259b2a2aa2668d4
Hilali Abdel <abdel.d.hilali@gmail.com>
2024-10-17 00:55:00 +0100

    Update auth_controllers.js

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -2,7 +2,7 @@ import {Client,Account,ID,Functions} from "appwrite"
 
 const client = new Client()
 .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-.setProject('<YOUR-PROJECT-ID>')              // Your project ID
+.setProject('6627279a395b9e5ea090')              // Your project ID
 
 const account = new Account(client);
 
